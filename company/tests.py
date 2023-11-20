275652dcc4194a939a99828ae2a04af12ea0022b
yawtalkstechs <yawtalkstechs@gmail.com>
2023-11-20 21:00:44 +0000

    Company and Products tested successfully

--- company/tests.py
+++ company/tests.py
@@ -1,3 +1,36 @@
+# company/tests.py
 from django.test import TestCase
+from .models import Company
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class CompanyModelTest(TestCase):
+    def setUp(self):
+        # Create a user associated with the company
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.company = Company.objects.create(
+            name='Test Company',
+            registration_date='2023-11-20',
+            address='Test Address',
+            phone='123-456-7890',
+            email='test@example.com',
+            logo_url='http://www.testcompany.com/logo.png',
+        )
+        self.user.company = self.company
+        self.user.save()
+
+    def test_company_creation(self):
+        """Test the creation of a company."""
+        self.assertEqual(self.company.__str__(), 'Test Company')
+        self.assertEqual(self.company.name, 'Test Company')
+        self.assertEqual(self.company.registration_date.strftime('%Y-%m-%d'), '2023-11-20')
+        self.assertEqual(self.company.address, 'Test Address')
+        self.assertEqual(self.company.phone, '123-456-7890')
+        self.assertEqual(self.company.email, 'test@example.com')
+        self.assertEqual(self.company.logo_url, 'http://www.testcompany.com/logo.png')
+
+    def test_company_user_association(self):
+        """Test the association of a user with a company."""
+        self.assertEqual(self.user.company, self.company)
--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,54 @@
+# products/tests.py
 from django.test import TestCase
+from .models import Category, Product
+from company.models import Company
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class ProductModelTest(TestCase):
+    def setUp(self):
+        # Create a company and a user associated with that company
+        self.company = Company.objects.create(
+            name='Test Company',
+            registration_date='2023-01-01',
+            address='Test Address',
+            phone='123-456-7890',
+            email='test@example.com',
+            logo_url='http://www.testcompany.com/logo.png',
+        )
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='testpassword',
+        )
+        self.user.company = self.company
+        self.user.save()
+
+        # Create a category
+        self.category = Category.objects.create(name='Test Category')
+
+        # Create a product
+        self.product = Product.objects.create(
+            company=self.company,
+            category=self.category,
+            name='Test Product',
+            slug='test-product',
+            description='Test description',
+            price=19.99,
+            stock_quantity=100,
+            image_url='http://www.testproduct.com/image.png',
+            available=True,
+        )
+
+    def test_product_creation(self):
+        """Test the creation of a product."""
+        self.assertEqual(self.product.__str__(), 'Test Product')
+        self.assertEqual(self.product.company, self.company)
+        self.assertEqual(self.product.category, self.category)
+        self.assertEqual(self.product.name, 'Test Product')
+        self.assertEqual(self.product.slug, 'test-product')
+        self.assertEqual(self.product.description, 'Test description')
+        self.assertEqual(self.product.price, 19.99)
+        self.assertEqual(self.product.stock_quantity, 100)
+        self.assertEqual(self.product.image_url, 'http://www.testproduct.com/image.png')
+        self.assertTrue(self.product.available)
+        self.assertIsNotNone(self.product.created)
+        self.assertIsNotNone(self.product.updated)
